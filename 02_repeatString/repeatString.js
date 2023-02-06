4c5936ea62b3172a65a31058cf6b5137e521b241
beejayofgod <adexbolaji100@gmail.com>
2023-02-07 00:39:00 +0100

    a function that returns 'heyheyhey'

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+// let msg, time;
+let userMsg = "";
+const repeatString = function (msg, time) {
+    for (let i = 0; i < time; i++) {
+        userMsg += msg;
+    }
+    return userMsg;  return userMsg;
 };
 
 // Do not edit below this line
