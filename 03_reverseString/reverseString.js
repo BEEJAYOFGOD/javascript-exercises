229026e302a238c9849e7807eb8c7ae7fbe9cebb
beejayofgod <adexbolaji100@gmail.com>
2023-02-07 01:37:04 +0100

    created a reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function (userMsg) {
+    let stringed = ""; //initialization of the reversed string
+    for (let count = 1; count < userMsg.length + 1; count++) {
+        /*
+         * A for loop that loops through the
+         * number of elements iin the string &
+         * adds them to the empty string
+         */
+        stringed += userMsg[userMsg.length - count];
+    }
+    return stringed;
 };
 
 // Do not edit below this line
